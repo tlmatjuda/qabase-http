84304e91510694a3804b358d985992b588850e9d
Thabo Matjuda <tl.matjuda@gmail.com>
2025-02-06 10:24:35 +0400

    Initial commit

---
+++ src/main/kotlin/com/toob/qabase_http/QabaseHttpApplication.kt
@@ -0,0 +1,11 @@
+package com.toob.qabase_http
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QabaseHttpApplication
+
+fun main(args: Array<String>) {
+	runApplication<QabaseHttpApplication>(*args)
+}
---
+++ src/test/kotlin/com/toob/qabase_http/QabaseHttpApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toob.qabase_http
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QabaseHttpApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
